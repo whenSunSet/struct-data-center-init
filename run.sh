cb8e9eaffaaa1f690d1a6f2ba7b8d522342789ab
WhenSunset <1018998632@gmail.com>
2022-08-11 16:47:55 +0800

    update

--- run.sh
+++ run.sh
@@ -5,18 +5,6 @@ cd ../ParseServer
 sudo sh run.sh
 
 cd ../MetaBase
-mkdir data/db/pg_commit_ts
-mkdir data/db/pg_dynshmem
-mkdir data/db/pg_logical/mappings
-mkdir data/db/pg_logical/snapshots
-mkdir data/db/pg_replslot
-mkdir data/db/pg_serial
-mkdir data/db/pg_snapshots
-mkdir data/db/pg_stat
-mkdir data/db/pg_stat_tmp
-mkdir data/db/pg_tblspc
-mkdir data/db/pg_twophase
-mkdir data/db/pg_wal/archive_status
 sudo sh run.sh
 
 cd ../n8n
